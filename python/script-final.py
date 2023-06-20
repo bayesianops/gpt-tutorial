963d748751500b0b74f9e395448c58b2e9dfd95e
Daniel Lee <bearlee@alum.mit.edu>
2023-06-19 20:09:26 -0400

    adding 02

--- python/script-final.py
+++ python/script-final.py
@@ -97,9 +97,6 @@ if (verbose):
 
 
 ## Start of Language models
-vocab_size = len(set(text))   # total number of characters in the text
-batch_size = 32  # how many independent sequences will we process in parallel;  B
-block_size = 8   # what is the maximum context length for predictions?;         T
 
 
 ############################################################
@@ -107,8 +104,13 @@ block_size = 8   # what is the maximum context length for predictions?;         
 model_01 = CmdStanModel(stan_file=os.path.join('..', 'stan', '01-bigram.stan'))
 
 
+vocab_size = len(set(text))   # total number of characters in the text
+batch_size = 32  # how many independent sequences will we process in parallel;  B
+block_size = 8   # what is the maximum context length for predictions?;         T
+
 xb, yb = get_data_batch(data_train, batch_size, block_size)
 xb_val, yb_val = get_data_batch(data_val, batch_size, block_size)
+
 data = {
     'vocab_size': vocab_size,
     'batch_size': batch_size,
@@ -121,8 +123,12 @@ data = {
 }
 
 
-## This is wrong. Why? 
+## This is wrong. Why?
+## Look at loss and loss_val
 optimum_01 = model_01.optimize(data=data, show_console=True, algorithm="LBFGS")
+print(optimum_01.stan_variable('loss'))
+print(optimum_01.stan_variable('loss_validation'))   
+
 print(decode(optimum_01.stan_variable('new_tokens')))
 
 gq_01 = model_01.generate_quantities(data=data, previous_fit=optimum_01)
@@ -146,8 +152,55 @@ for step in range(1000):
 
 print(optimum_01.stan_variable('loss'))
 print(optimum_01.stan_variable('loss_validation'))   
-    
+
 print(decode(optimum_01.stan_variable('new_tokens')))
 
 gq_01 = model_01.generate_quantities(data=data, previous_fit=optimum_01)
 print(decode(gq_01.stan_variable('new_tokens')[0]))
+
+
+
+############################################################
+## 02: embedding
+model_02 = CmdStanModel(stan_file=os.path.join('..', 'stan', '02-different-embedding-size.stan')
+
+vocab_size = len(set(text))   # total number of characters in the text
+batch_size = 32  # how many independent sequences will we process in parallel;  B
+block_size = 8   # what is the maximum context length for predictions?;         T
+n_embed = 32     # embedding size                        
+
+xb, yb = get_data_batch(data_train, batch_size, block_size)
+xb_val, yb_val = get_data_batch(data_val, batch_size, block_size)
+data = {
+    'vocab_size': vocab_size,
+    'batch_size': batch_size,
+    'block_size': block_size,
+    'n_embed': n_embed,
+    'xb': xb,
+    'yb': yb,
+    'xb_val': xb_val,
+    'yb_val': yb_val,
+    'max_new_tokens': 500
+}
+
+optimum_02 = model_02.optimize(data=data, show_console=True, iter=1, init_alpha=0.0001, algorithm="LBFGS")
+
+for step in range(1000):
+    print("step = ", step)
+    xb, yb = get_data_batch(data_train, batch_size, block_size)
+    xb_val, yb_val = get_data_batch(data_val, batch_size, block_size)
+    data['xb'] = xb
+    data['yb'] = yb
+    data['xb_val'] = xb_val
+    data['yb_val'] = yb_val
+    optimum_02 = model_02.optimize(data = data, show_console=(step % 100 == 0),
+                                   iter=1, init_alpha=0.0001, algorithm="LBFGS",
+                                   inits=optimum_02.stan_variables())
+
+print(optimum_02.stan_variable('loss'))
+print(optimum_02.stan_variable('loss_validation'))   
+    
+print(decode(optimum_02.stan_variable('new_tokens')))
+
+gq_02 = model_02.generate_quantities(data=data, previous_fit=optimum_02)
+print(decode(gq_02.stan_variable('new_tokens')[0]))
--- stan/02-different-embedding-size.stan
+++ stan/02-different-embedding-size.stan
@@ -1,7 +1,13 @@
+functions {
+  vector lm_head(vector x, matrix A, vector b) {
+    return A * x + b;
+  }
+}
 data {
   int<lower = 1> vocab_size;           // e.g. 65
   int<lower = 1> batch_size;           // e.g. 32
   int<lower = 1> block_size;           // e.g. 8
+  int<lower = 1> n_embed;              // e.g. 32
   array[batch_size, block_size] int<lower = 1, upper = vocab_size> xb;
   array[batch_size, block_size] int<lower = 1, upper = vocab_size> yb;
 
@@ -11,13 +17,16 @@ data {
   int<lower = 0> max_new_tokens;
 }
 parameters {
-  array[vocab_size] vector[vocab_size] token_embedding;
+  array[vocab_size] vector[n_embed] token_embedding;        // change in token_embedding size
+  matrix[vocab_size, n_embed] lm_head_multiplier;           // new parameters
+  vector[vocab_size] lm_head_offset;                        // new parameters
 }
 transformed parameters {
   real loss = 0;
   for (b in 1:batch_size) {
     for (t in 1:block_size) {
-      loss += categorical_logit_lpmf(yb[b, t] | token_embedding[xb[b, t]]);
+      vector[vocab_size] logits = lm_head(token_embedding[xb[b, t]], lm_head_multiplier, lm_head_offset);
+      loss += categorical_logit_lpmf(yb[b, t] | logits);
     }
   }
   loss /= batch_size * block_size;
@@ -29,7 +38,8 @@ generated quantities {
   real loss_validation = 0;
   for (b in 1:batch_size) {
     for (t in 1:block_size) {
-      loss_validation += categorical_logit_lpmf(yb_val[b, t] | token_embedding[xb_val[b, t]]);
+      vector[vocab_size] logits = lm_head(token_embedding[xb_val[b, t]], lm_head_multiplier, lm_head_offset);
+      loss_validation += categorical_logit_lpmf(yb_val[b, t] | logits);
     }
   }
   loss_validation /= batch_size * block_size;
@@ -37,9 +47,10 @@ generated quantities {
   print("train loss ", -loss, ", val loss ", -loss_validation);
   print("************************************************************");
   
+  
   array[max_new_tokens] int<lower = 1, upper = vocab_size> new_tokens;
   new_tokens[1] = 1;
   for (n in 2:max_new_tokens) {
-    new_tokens[n] = categorical_logit_rng(token_embedding[new_tokens[n - 1]]);
+    new_tokens[n] = categorical_logit_rng(lm_head(token_embedding[new_tokens[n - 1]], lm_head_multiplier, lm_head_offset));
   }
 }
